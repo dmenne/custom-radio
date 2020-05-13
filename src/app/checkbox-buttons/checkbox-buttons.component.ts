9e1e7c7aeae4f8db5c9b83bd2d310e97dedd9b18
Dieter Menne <dieter.menne@menne-biomed.de>
2020-05-13 16:50:47 +0200

    Added, but not integrated checkbox-buttons

---
+++ src/app/checkbox-buttons/checkbox-buttons.component.ts
@@ -0,0 +1,76 @@
+import { Component, EventEmitter, Input, OnInit, Output } from '@angular/core'
+import { FormioCustomComponent } from 'angular-formio'
+
+@Component({
+  selector: 'app-radio-buttons',
+  templateUrl: './checkbox-buttons.component.html',
+  styleUrls: ['./checkbox-buttons.component.scss']
+
+})
+
+export class CheckboxBoxButtonsComponent  implements FormioCustomComponent<string>, OnInit {
+
+  @Input()
+  value: string
+
+  @Input()
+  type: string
+
+  @Input()
+  input: boolean
+
+  @Input()
+  key: string
+
+  @Input()
+  label: string
+
+  @Output()
+  valueChange = new EventEmitter<string>()
+
+  @Input()
+  values: {}
+
+  @Input()
+  disabled: boolean
+
+  @Input()
+  hidden: boolean
+
+  @Input()
+  buttonStyle: string
+
+  @Input()
+  inline: boolean ;
+
+
+  ngOnInit() {
+    console.log('radio-buttons onInit')
+  }
+
+  showGroup() {
+    return  !this.hidden && this.values && ((this.values[0].value) !== '');
+  }
+
+  isActive(val: string) {
+    return this.value && (val === this.value)
+  }
+
+
+  buttonClass(val: string) {
+    const btn =
+      'btn mr-1  mb-2  btn-outline-'  +
+      this.buttonStyle + ' ' +
+      (this.isActive(val) ? 'active' : '') +
+      (this.inline ? ' btn-group-rounded' : '')
+    return btn
+  }
+
+
+  updateValue(payload: string) {
+    console.log(payload)
+    this.value = payload;
+    this.valueChange.emit(payload);
+  }
+
+}
---
+++ src/app/checkbox-buttons/checkbox-buttons.formio.ts
@@ -0,0 +1,39 @@
+import { Injector } from '@angular/core'
+import {
+  FormioCustomComponentInfo,
+  registerCustomFormioComponent,
+  Components
+} from 'angular-formio'
+import { CheckboxBoxButtonsComponent } from './checkbox-buttons.component'
+import checkboxesEditFormJson from '../../assets/radio-buttons.edit.json'
+
+function checkboxesEditForm() {
+  // Base form is created from
+  console.log(JSON.stringify(Components.components.radio.editForm()))
+  // Get the output from the console, format it with Notepad++ Tools/JSTool/JSFormat
+  // and store it as a file in ../../assets.
+  // Make changes to your Edit form
+  // Before deployment, compact JSON file with Notepad++ Tools/JSToool/JSMin
+  return checkboxesEditFormJson
+}
+
+const COMPONENT_OPTIONS: FormioCustomComponentInfo = {
+  type: 'checkboxbuttons', // custom type. Formio will identify the field with this type.
+  selector: 'checkbox-buttons', // custom selector. Angular Elements will create a custom html tag with this selector
+  title: 'Multi Checkbox Buttons', // Title of the component
+  group: 'basic', // Build Group
+  icon: 'check-circle', // Icon
+  fieldOptions: ['label', 'values', 'key', 'hidden', 'buttonStyle', 'inline'],
+  editForm: checkboxesEditForm, // Use editForm from Radio buttons
+  //  template: 'input', // Optional: define a template for the element. Default: input
+//  changeEvent: 'valueChange', // Optional: define the changeEvent when the formio updates the value in the state. Default: 'valueChange',
+//  documentation: '', // Optional: define the documentation of the field
+//  weight: 0, // Optional: define the weight in the builder group
+//  schema: {}, // Optional: define extra default schema for the field
+//  extraValidators: [], // Optional: define extra validators  for the field
+//  emptyValue: '' // Optional: the emptyValue of the field
+}
+
+export function registerCheckboxButtonsComponent(injector: Injector) {
+  registerCustomFormioComponent(COMPONENT_OPTIONS, CheckboxBoxButtonsComponent, injector)
+}
