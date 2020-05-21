5b977cc3a6b2f6bf7f234251490f3830d9981c7e
Dieter Menne <dieter.menne@menne-biomed.de>
2020-05-21 19:21:24 +0200

    Working cypress drag-drop test

--- cypress/integration/spec.ts
+++ cypress/integration/spec.ts
@@ -2,12 +2,12 @@ describe('When Formio Radio Buttons starting page is loaded', () => {
   beforeEach(() => {
     cy.visit('/')
   })
-  it ('Has Title and menu', () => {
+  xit ('Has Title and menu', () => {
     cy.contains('Formio Custom Buttons Test')
     cy.contains('Drag and Drop a form component')
     cy.contains('Submit')
   })
-  it ('Has Buttons', () => {
+  xit ('Has Buttons', () => {
     cy.get('[data-key="textfield"]').should('have.class', 'formcomponent')
     cy.get('[data-key="multicheckbuttons"]').should('have.class', 'formcomponent')
     cy.get('[data-key="checkbox"]').should('have.class', 'formcomponent')
@@ -15,6 +15,22 @@ describe('When Formio Radio Buttons starting page is loaded', () => {
     cy.get('[data-key="yesnobuttons"]').should('have.class', 'formcomponent')
     cy.contains('Drag and Drop').should('have.class', 'drag-and-drop-alert')
   })
+
   it('Can use Drag-Drop', () => {
-  })
+    cy.get('[data-type="textfield"]')
+      .trigger('mousedown', {which: 1})
+      .trigger('mousemove', {which: 1, clientX: 400, clientY: 60})
+      .trigger('mouseup', {force: true})
+      })
+
+  xit('Can select panel', () => {
+    cy.contains('My Basics')
+      .trigger('click', { button: 0 })
+    cy.get('[data-key="textfield"]').should('be.visible')
+    cy.get('[data-key="multicheckbox"]').should('not.be.visible')
+    cy.contains('Big Checks')
+      .trigger('click', { button: 0 })
+    cy.get('[data-key="textfield"]').should('not.be.visible')
+  }
+  )
 })
--- cypress/plugins/index.js
+++ cypress/plugins/index.js
@@ -1,5 +1,7 @@
 const cypressTypeScriptPreprocessor = require('./cy-ts-preprocessor');
+const initCypressMousePositionPlugin = require('cypress-mouse-position/plugin');
 
 module.exports = on => {
   on('file:preprocessor', cypressTypeScriptPreprocessor);
+  initCypressMousePositionPlugin(on);
 };
